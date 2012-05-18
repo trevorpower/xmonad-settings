73c37f43afd8838f86edca05bf4f126743d2bfc8
Trevor Power <trevor_power@yahoo.com>
2012-05-18 10:29:07 -0700

    added some tweaks specific to screencast recordig

--- xmonad.hs
+++ xmonad.hs
@@ -1,16 +1,24 @@
 import XMonad
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace
+import XMonad.Layout.Gaps
 
 main = do
 	xmonad $ defaultConfig
           { handleEventHook = fullscreenEventHook
           , normalBorderColor = "#073642"
           , focusedBorderColor = "#cb4b16"
-          , layoutHook = layout
+          , layoutHook = onWorkspace "9" screencast $
+                         layout
           }
 
-layout = tiled ||| Mirror tiled ||| noBorders(Full)
+-- for screen casts, to get native 16:9 aspect ratio
+-- the following gives me 16000 X 9000 on my current desktop
+screencast = gaps [(R, 40), (L, 40), (D, 75), (U, 75)]$ layout
+
+--layout = tiled ||| Mirror tiled ||| noBorders(Full) 
+layout = tiled ||| noBorders(Full) 
   where
      tiled   = Tall nmaster delta ratio
      nmaster = 1     -- number of windows
