0c99f4de3df0f0b4524a7a5b9f14bf51981998b0
Trevor Power <trevor_power@yahoo.com>
2012-06-01 03:03:24 -0700

    added layout for screencasts

--- xmonad.hs
+++ xmonad.hs
@@ -7,18 +7,18 @@ import XMonad.Layout.Gaps
 main = do
 	xmonad $ defaultConfig
           { handleEventHook = fullscreenEventHook
-          , normalBorderColor = "#073642"
-          , focusedBorderColor = "#cb4b16"
+          , terminal = "urxvt"
+          , normalBorderColor = "#282a2e"
+          , focusedBorderColor = "#de935f"
           , layoutHook = onWorkspace "9" screencast $
                          layout
           }
 
 -- for screen casts, to get native 16:9 aspect ratio
--- the following gives me 16000 X 9000 on my current desktop
+-- the following gives me 1600 X 900 on my current desktop
 screencast = gaps [(R, 40), (L, 40), (D, 75), (U, 75)]$ layout
 
---layout = tiled ||| Mirror tiled ||| noBorders(Full) 
-layout = tiled ||| noBorders(Full) 
+layout = tiled ||| Mirror tiled ||| noBorders(Full)
   where
      tiled   = Tall nmaster delta ratio
      nmaster = 1     -- number of windows
