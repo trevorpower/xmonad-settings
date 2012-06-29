330ee3168fbf814de639df4e3c6fc35394a18c49
Trevor Power <trevor_power@yahoo.com>
2012-06-29 16:48:02 +0100

    changed no borders logic

--- xmonad.hs
+++ xmonad.hs
@@ -18,7 +18,7 @@ main = do
 -- the following gives me 1600 X 900 on my current desktop
 screencast = gaps [(R, 40), (L, 40), (D, 75), (U, 75)]$ layout
 
-layout = tiled ||| Mirror tiled ||| noBorders(Full)
+layout = smartBorders(tiled ||| Mirror tiled ||| Full)
   where
      tiled   = Tall nmaster delta ratio
      nmaster = 1     -- number of windows
