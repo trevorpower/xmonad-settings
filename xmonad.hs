7832dba0fec9d9e7bc0cf4da44df24dd8098b598
Trevor Power <trevor_power@yahoo.com>
2012-09-07 20:35:30 +0100

    changed whitespace/layout

--- xmonad.hs
+++ xmonad.hs
@@ -5,14 +5,15 @@ import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Gaps
 
 main = do
-	xmonad $ defaultConfig
-          { handleEventHook = fullscreenEventHook
-          , terminal = "urxvt"
-          , normalBorderColor = "#282a2e"
-          , focusedBorderColor = "#de935f"
-          , layoutHook = onWorkspace "9" screencast $
-                         layout
-          }
+  xmonad $ defaultConfig
+    {
+      handleEventHook = fullscreenEventHook,
+      terminal = "urxvt",
+      normalBorderColor = "#282a2e",
+      focusedBorderColor = "#de935f",
+      layoutHook = onWorkspace "9" screencast $
+                   layout
+    }
 
 -- for screen casts, to get native 16:9 aspect ratio
 -- the following gives me 1600 X 900 on my current desktop
@@ -20,7 +21,7 @@ screencast = gaps [(R, 40), (L, 40), (D, 75), (U, 75)]$ layout
 
 layout = smartBorders(tiled ||| Mirror tiled ||| Full)
   where
-     tiled   = Tall nmaster delta ratio
-     nmaster = 1     -- number of windows
-     ratio   = 1/2   -- default 
-     delta   = 3/100 -- adjust by
+    tiled   = Tall nmaster delta ratio
+    nmaster = 1     -- number of windows
+    ratio   = 1/2   -- default 
+    delta   = 3/100 -- adjust by
